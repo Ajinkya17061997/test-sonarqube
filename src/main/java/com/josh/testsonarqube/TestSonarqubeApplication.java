63615afad52edb7b1c5ce83520b5860dff2e0cc8
ajinkya <ajinkyawaghdare97@gmail.com>
2022-08-11 14:51:01 +0530

    Initial commit

---
+++ src/main/java/com/josh/testsonarqube/TestSonarqubeApplication.java
@@ -0,0 +1,19 @@
+package com.josh.testsonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSonarqubeApplication {
+
+	public static void doTest(){
+		System.out.println("This is thing");
+		Object o = null;
+		System.out.println(o.toString());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSonarqubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josh/testsonarqube/TestSonarqubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.testsonarqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSonarqubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
